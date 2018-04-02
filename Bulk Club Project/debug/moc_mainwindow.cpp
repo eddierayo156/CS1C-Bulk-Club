f0b2a438f1ebe04a1b24c89693520bd76bd9a642
eddierayo156 <36017104+eddierayo156@users.noreply.github.com>
2018-04-02 12:16:33 -0700

    Add files via upload

---
+++ Bulk Club Project/debug/moc_mainwindow.cpp
@@ -0,0 +1,380 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.8.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[57];
+    char stringdata0[1630];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 27), // "on_pushButton_login_clicked"
+QT_MOC_LITERAL(2, 39, 0), // ""
+QT_MOC_LITERAL(3, 40, 29), // "on_pushButton_members_clicked"
+QT_MOC_LITERAL(4, 70, 31), // "on_pushButton_inventory_clicked"
+QT_MOC_LITERAL(5, 102, 33), // "on_pushButton_memberships_cli..."
+QT_MOC_LITERAL(6, 136, 26), // "on_pushButton_back_clicked"
+QT_MOC_LITERAL(7, 163, 27), // "on_pushButton_sales_clicked"
+QT_MOC_LITERAL(8, 191, 31), // "on_pushButton_purchases_clicked"
+QT_MOC_LITERAL(9, 223, 25), // "on_pushButton_qty_clicked"
+QT_MOC_LITERAL(10, 249, 29), // "on_pushButton_rebates_clicked"
+QT_MOC_LITERAL(11, 279, 33), // "on_pushButton_expirations_cli..."
+QT_MOC_LITERAL(12, 313, 33), // "on_pushButton_item_lookup_cli..."
+QT_MOC_LITERAL(13, 347, 35), // "on_pushButton_member_lookup_c..."
+QT_MOC_LITERAL(14, 383, 29), // "on_pushButton_convert_clicked"
+QT_MOC_LITERAL(15, 413, 36), // "on_pushButton_back_inventory_..."
+QT_MOC_LITERAL(16, 450, 38), // "on_pushButton_back_membership..."
+QT_MOC_LITERAL(17, 489, 33), // "on_pushButton_back_report_cli..."
+QT_MOC_LITERAL(18, 523, 36), // "on_pushButton_back_purchases_..."
+QT_MOC_LITERAL(19, 560, 30), // "on_pushButton_back_qty_clicked"
+QT_MOC_LITERAL(20, 591, 34), // "on_pushButton_back_rebates_cl..."
+QT_MOC_LITERAL(21, 626, 38), // "on_pushButton_back_expiration..."
+QT_MOC_LITERAL(22, 665, 38), // "on_pushButton_back_item_looku..."
+QT_MOC_LITERAL(23, 704, 40), // "on_pushButton_back_member_loo..."
+QT_MOC_LITERAL(24, 745, 34), // "on_pushButton_back_convert_cl..."
+QT_MOC_LITERAL(25, 780, 34), // "on_pushButton_logout_admin_cl..."
+QT_MOC_LITERAL(26, 815, 36), // "on_pushButton_logout_manager_..."
+QT_MOC_LITERAL(27, 852, 29), // "on_pushButton_addItem_clicked"
+QT_MOC_LITERAL(28, 882, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(29, 904, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(30, 928, 26), // "on_addMemberButton_clicked"
+QT_MOC_LITERAL(31, 955, 31), // "on_pushButton_addFinish_clicked"
+QT_MOC_LITERAL(32, 987, 30), // "on_SearchMembDowngrade_clicked"
+QT_MOC_LITERAL(33, 1018, 28), // "on_SearchMembUpgrade_clicked"
+QT_MOC_LITERAL(34, 1047, 26), // "on_pushButton_Exit_clicked"
+QT_MOC_LITERAL(35, 1074, 28), // "on_pushButton_Delete_clicked"
+QT_MOC_LITERAL(36, 1103, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(37, 1127, 23), // "on_pushButton_3_clicked"
+QT_MOC_LITERAL(38, 1151, 23), // "on_pushButton_5_clicked"
+QT_MOC_LITERAL(39, 1175, 32), // "on_pushButton_removeItem_clicked"
+QT_MOC_LITERAL(40, 1208, 23), // "on_pushButton_6_clicked"
+QT_MOC_LITERAL(41, 1232, 19), // "on_LoadData_clicked"
+QT_MOC_LITERAL(42, 1252, 23), // "on_pushButton_7_clicked"
+QT_MOC_LITERAL(43, 1276, 23), // "on_pushButton_8_clicked"
+QT_MOC_LITERAL(44, 1300, 23), // "on_pushButton_9_clicked"
+QT_MOC_LITERAL(45, 1324, 24), // "on_pushButton_10_clicked"
+QT_MOC_LITERAL(46, 1349, 24), // "on_pushButton_11_clicked"
+QT_MOC_LITERAL(47, 1374, 24), // "on_pushButton_13_clicked"
+QT_MOC_LITERAL(48, 1399, 24), // "on_pushButton_14_clicked"
+QT_MOC_LITERAL(49, 1424, 24), // "on_pushButton_15_clicked"
+QT_MOC_LITERAL(50, 1449, 24), // "on_pushButton_12_clicked"
+QT_MOC_LITERAL(51, 1474, 24), // "on_pushButton_16_clicked"
+QT_MOC_LITERAL(52, 1499, 30), // "on_pushButton_purchase_clicked"
+QT_MOC_LITERAL(53, 1530, 24), // "on_pushButton_17_clicked"
+QT_MOC_LITERAL(54, 1555, 24), // "on_pushButton_18_clicked"
+QT_MOC_LITERAL(55, 1580, 24), // "on_pushButton_19_clicked"
+QT_MOC_LITERAL(56, 1605, 24) // "on_pushButton_20_clicked"
+
+    },
+    "MainWindow\0on_pushButton_login_clicked\0"
+    "\0on_pushButton_members_clicked\0"
+    "on_pushButton_inventory_clicked\0"
+    "on_pushButton_memberships_clicked\0"
+    "on_pushButton_back_clicked\0"
+    "on_pushButton_sales_clicked\0"
+    "on_pushButton_purchases_clicked\0"
+    "on_pushButton_qty_clicked\0"
+    "on_pushButton_rebates_clicked\0"
+    "on_pushButton_expirations_clicked\0"
+    "on_pushButton_item_lookup_clicked\0"
+    "on_pushButton_member_lookup_clicked\0"
+    "on_pushButton_convert_clicked\0"
+    "on_pushButton_back_inventory_clicked\0"
+    "on_pushButton_back_memberships_clicked\0"
+    "on_pushButton_back_report_clicked\0"
+    "on_pushButton_back_purchases_clicked\0"
+    "on_pushButton_back_qty_clicked\0"
+    "on_pushButton_back_rebates_clicked\0"
+    "on_pushButton_back_expirations_clicked\0"
+    "on_pushButton_back_item_lookup_clicked\0"
+    "on_pushButton_back_member_lookup_clicked\0"
+    "on_pushButton_back_convert_clicked\0"
+    "on_pushButton_logout_admin_clicked\0"
+    "on_pushButton_logout_manager_clicked\0"
+    "on_pushButton_addItem_clicked\0"
+    "on_pushButton_clicked\0on_pushButton_2_clicked\0"
+    "on_addMemberButton_clicked\0"
+    "on_pushButton_addFinish_clicked\0"
+    "on_SearchMembDowngrade_clicked\0"
+    "on_SearchMembUpgrade_clicked\0"
+    "on_pushButton_Exit_clicked\0"
+    "on_pushButton_Delete_clicked\0"
+    "on_pushButton_4_clicked\0on_pushButton_3_clicked\0"
+    "on_pushButton_5_clicked\0"
+    "on_pushButton_removeItem_clicked\0"
+    "on_pushButton_6_clicked\0on_LoadData_clicked\0"
+    "on_pushButton_7_clicked\0on_pushButton_8_clicked\0"
+    "on_pushButton_9_clicked\0"
+    "on_pushButton_10_clicked\0"
+    "on_pushButton_11_clicked\0"
+    "on_pushButton_13_clicked\0"
+    "on_pushButton_14_clicked\0"
+    "on_pushButton_15_clicked\0"
+    "on_pushButton_12_clicked\0"
+    "on_pushButton_16_clicked\0"
+    "on_pushButton_purchase_clicked\0"
+    "on_pushButton_17_clicked\0"
+    "on_pushButton_18_clicked\0"
+    "on_pushButton_19_clicked\0"
+    "on_pushButton_20_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      55,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  289,    2, 0x08 /* Private */,
+       3,    0,  290,    2, 0x08 /* Private */,
+       4,    0,  291,    2, 0x08 /* Private */,
+       5,    0,  292,    2, 0x08 /* Private */,
+       6,    0,  293,    2, 0x08 /* Private */,
+       7,    0,  294,    2, 0x08 /* Private */,
+       8,    0,  295,    2, 0x08 /* Private */,
+       9,    0,  296,    2, 0x08 /* Private */,
+      10,    0,  297,    2, 0x08 /* Private */,
+      11,    0,  298,    2, 0x08 /* Private */,
+      12,    0,  299,    2, 0x08 /* Private */,
+      13,    0,  300,    2, 0x08 /* Private */,
+      14,    0,  301,    2, 0x08 /* Private */,
+      15,    0,  302,    2, 0x08 /* Private */,
+      16,    0,  303,    2, 0x08 /* Private */,
+      17,    0,  304,    2, 0x08 /* Private */,
+      18,    0,  305,    2, 0x08 /* Private */,
+      19,    0,  306,    2, 0x08 /* Private */,
+      20,    0,  307,    2, 0x08 /* Private */,
+      21,    0,  308,    2, 0x08 /* Private */,
+      22,    0,  309,    2, 0x08 /* Private */,
+      23,    0,  310,    2, 0x08 /* Private */,
+      24,    0,  311,    2, 0x08 /* Private */,
+      25,    0,  312,    2, 0x08 /* Private */,
+      26,    0,  313,    2, 0x08 /* Private */,
+      27,    0,  314,    2, 0x08 /* Private */,
+      28,    0,  315,    2, 0x08 /* Private */,
+      29,    0,  316,    2, 0x08 /* Private */,
+      30,    0,  317,    2, 0x08 /* Private */,
+      31,    0,  318,    2, 0x08 /* Private */,
+      32,    0,  319,    2, 0x08 /* Private */,
+      33,    0,  320,    2, 0x08 /* Private */,
+      34,    0,  321,    2, 0x08 /* Private */,
+      35,    0,  322,    2, 0x08 /* Private */,
+      36,    0,  323,    2, 0x08 /* Private */,
+      37,    0,  324,    2, 0x08 /* Private */,
+      38,    0,  325,    2, 0x08 /* Private */,
+      39,    0,  326,    2, 0x08 /* Private */,
+      40,    0,  327,    2, 0x08 /* Private */,
+      41,    0,  328,    2, 0x08 /* Private */,
+      42,    0,  329,    2, 0x08 /* Private */,
+      43,    0,  330,    2, 0x08 /* Private */,
+      44,    0,  331,    2, 0x08 /* Private */,
+      45,    0,  332,    2, 0x08 /* Private */,
+      46,    0,  333,    2, 0x08 /* Private */,
+      47,    0,  334,    2, 0x08 /* Private */,
+      48,    0,  335,    2, 0x08 /* Private */,
+      49,    0,  336,    2, 0x08 /* Private */,
+      50,    0,  337,    2, 0x08 /* Private */,
+      51,    0,  338,    2, 0x08 /* Private */,
+      52,    0,  339,    2, 0x08 /* Private */,
+      53,    0,  340,    2, 0x08 /* Private */,
+      54,    0,  341,    2, 0x08 /* Private */,
+      55,    0,  342,    2, 0x08 /* Private */,
+      56,    0,  343,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_login_clicked(); break;
+        case 1: _t->on_pushButton_members_clicked(); break;
+        case 2: _t->on_pushButton_inventory_clicked(); break;
+        case 3: _t->on_pushButton_memberships_clicked(); break;
+        case 4: _t->on_pushButton_back_clicked(); break;
+        case 5: _t->on_pushButton_sales_clicked(); break;
+        case 6: _t->on_pushButton_purchases_clicked(); break;
+        case 7: _t->on_pushButton_qty_clicked(); break;
+        case 8: _t->on_pushButton_rebates_clicked(); break;
+        case 9: _t->on_pushButton_expirations_clicked(); break;
+        case 10: _t->on_pushButton_item_lookup_clicked(); break;
+        case 11: _t->on_pushButton_member_lookup_clicked(); break;
+        case 12: _t->on_pushButton_convert_clicked(); break;
+        case 13: _t->on_pushButton_back_inventory_clicked(); break;
+        case 14: _t->on_pushButton_back_memberships_clicked(); break;
+        case 15: _t->on_pushButton_back_report_clicked(); break;
+        case 16: _t->on_pushButton_back_purchases_clicked(); break;
+        case 17: _t->on_pushButton_back_qty_clicked(); break;
+        case 18: _t->on_pushButton_back_rebates_clicked(); break;
+        case 19: _t->on_pushButton_back_expirations_clicked(); break;
+        case 20: _t->on_pushButton_back_item_lookup_clicked(); break;
+        case 21: _t->on_pushButton_back_member_lookup_clicked(); break;
+        case 22: _t->on_pushButton_back_convert_clicked(); break;
+        case 23: _t->on_pushButton_logout_admin_clicked(); break;
+        case 24: _t->on_pushButton_logout_manager_clicked(); break;
+        case 25: _t->on_pushButton_addItem_clicked(); break;
+        case 26: _t->on_pushButton_clicked(); break;
+        case 27: _t->on_pushButton_2_clicked(); break;
+        case 28: _t->on_addMemberButton_clicked(); break;
+        case 29: _t->on_pushButton_addFinish_clicked(); break;
+        case 30: _t->on_SearchMembDowngrade_clicked(); break;
+        case 31: _t->on_SearchMembUpgrade_clicked(); break;
+        case 32: _t->on_pushButton_Exit_clicked(); break;
+        case 33: _t->on_pushButton_Delete_clicked(); break;
+        case 34: _t->on_pushButton_4_clicked(); break;
+        case 35: _t->on_pushButton_3_clicked(); break;
+        case 36: _t->on_pushButton_5_clicked(); break;
+        case 37: _t->on_pushButton_removeItem_clicked(); break;
+        case 38: _t->on_pushButton_6_clicked(); break;
+        case 39: _t->on_LoadData_clicked(); break;
+        case 40: _t->on_pushButton_7_clicked(); break;
+        case 41: _t->on_pushButton_8_clicked(); break;
+        case 42: _t->on_pushButton_9_clicked(); break;
+        case 43: _t->on_pushButton_10_clicked(); break;
+        case 44: _t->on_pushButton_11_clicked(); break;
+        case 45: _t->on_pushButton_13_clicked(); break;
+        case 46: _t->on_pushButton_14_clicked(); break;
+        case 47: _t->on_pushButton_15_clicked(); break;
+        case 48: _t->on_pushButton_12_clicked(); break;
+        case 49: _t->on_pushButton_16_clicked(); break;
+        case 50: _t->on_pushButton_purchase_clicked(); break;
+        case 51: _t->on_pushButton_17_clicked(); break;
+        case 52: _t->on_pushButton_18_clicked(); break;
+        case 53: _t->on_pushButton_19_clicked(); break;
+        case 54: _t->on_pushButton_20_clicked(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 55)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 55;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 55)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 55;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE

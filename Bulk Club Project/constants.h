4cc590afebd3ebdccc58b4ae4ef03eb7953d7151
eddierayo156 <36017104+eddierayo156@users.noreply.github.com>
2018-04-02 12:11:03 -0700

    Added constants for the database location

---
+++ Bulk Club Project/constants.h
@@ -0,0 +1,6 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int UNASSIGNED_DB_ID = -1;
+const QString DB_PATH = "C:/Users/erayo2/Desktop/Bulk Club/dbFinal.db"; // change to match your own path for the db
+#endif // CONSTANTS_H
